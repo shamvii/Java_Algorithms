e25f893e3f13b0d0d8449f06b4a81cb106d4c66e
Shambhavi Shukla <shamvii@users.noreply.github.com>
2017-05-11 03:04:17 -0400

    Add files via upload
    Singly LinkedList

---
+++ singlylinkedlist/Node.java
@@ -0,0 +1,14 @@
+package ds.singlylinkedlist;
+
+/**
+ * Created by shambhavishukla on 5/4/17.
+ */
+public class Node {
+
+    public int data;
+    public Node next;
+
+    public void displayNode() {
+        System.out.println("{" + data + "}");
+    }
+}
