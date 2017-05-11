3b0a932208a2d311c31f8f054c36f29f224014a5
Shambhavi Shukla <shamvii@users.noreply.github.com>
2017-05-11 03:09:22 -0400

    Recursion - Factorial
    Factorial using Recursion

---
+++ recursion/App.java
@@ -0,0 +1,7 @@
+package ds.recursion;
+
+/**
+ * Created by shambhavishukla on 5/6/17.
+ */
+public class App {
+}
---
+++ recursion/RecursiveFactorial.java
@@ -0,0 +1,13 @@
+package ds.recursion;
+
+/**
+ * Created by shambhavishukla on 5/7/17.
+ */
+public class RecursiveFactorial {
+
+    public static void main(String[] args) {
+        //System.out.println(RFactorial(5));
+
+    }
+}
+
